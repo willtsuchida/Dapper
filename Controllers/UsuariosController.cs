7ab6a266d5e276b8180002d2ae53b4671ed2339c
Willian Tsuchida <willian_hideki@hotmail.com>
2024-05-03 12:35:12 -0300

    dummy code, fim secao 04

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using eCommerce.API.Models;
+using eCommerce.API.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.API.Controllers
@@ -7,5 +9,48 @@ namespace eCommerce.API.Controllers
     [ApiController]
     public class UsuariosController : ControllerBase
     {
+        private IUsuarioRepository _usuarioRepository;
+        public UsuariosController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_usuarioRepository.Get());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _usuarioRepository.Get(id);
+            if (usuario == null) return NotFound("Usuário não encontrado!");
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody] Usuario usuario)
+        {
+            _usuarioRepository.Insert(usuario);
+            return Ok(usuario);
+        }
+
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Usuario usuario)
+        {
+            _usuarioRepository.Update(usuario);
+            return Ok(usuario);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Insert(int id)
+        {
+            _usuarioRepository.Delete(id);
+            return Ok();
+        }
+
     }
 }
---
+++ Repositories/UsuarioRepository.cs
@@ -0,0 +1,51 @@
+﻿using eCommerce.API.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        //Simulando banco
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario { Id = 1, Nome = "Filipe Rogrigues", Email = "filiperodrigues@gmail.com"},
+            new Usuario { Id = 2, Nome = "Marcelo Rogrigues", Email = "marcelo.rodrigues@gmail.com"},
+            new Usuario { Id = 3, Nome = "Jessica Rogrigues", Email = "jessica.rodrigues@gmail.com"}
+        };
+
+        public List<Usuario> Get()
+        {
+            return _db;
+        }
+
+        public Usuario Get(int id)
+        {
+            return _db.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            var ultimoUsuario = _db.LastOrDefault();
+            if (ultimoUsuario == null)
+            {
+                usuario.Id = 1;
+            }
+            else
+            {
+                usuario.Id = ultimoUsuario.Id;
+                usuario.Id++;
+            }
+            _db.Add(usuario);
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _db.Remove(_db.FirstOrDefault(x => x.Id == usuario.Id));
+            _db.Add(usuario);
+        }
+
+        public void Delete(int id)
+        {
+            _db.Remove(_db.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
